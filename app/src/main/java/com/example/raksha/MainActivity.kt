b5b7b6853a043f0c14eb53a9e14b662c4ea1cd97
Samarpita Das <samarpitabdas@gmail.com>
2024-06-17 20:08:02 +0530

    first commit

---
+++ app/src/main/java/com/example/raksha/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.raksha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.raksha.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.emergency -> replaceFragment(EmergencyFragment())
+                R.id.report -> replaceFragment(ReportFragment())
+                R.id.community -> replaceFragment(CommunityFragment())
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout, fragment)
+            .commit()
+    }
+}
